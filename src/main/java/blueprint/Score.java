1345caf1bf7f1ccc9d3a5edad242356b2b3d2eef
Tsaqif Mukhayyar <m3g3nz2@gmail.com>
2023-11-20 23:18:41 +0700

    :hammer: [fix, feat] add tic tac toe game, homeview, some interface :smiley:

---
+++ src/main/java/blueprint/Score.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package blueprint;
+
+/**
+ *
+ * @author user
+ */
+public interface Score {
+    void add();
+    void remove();
+    void sum();
+}
---
+++ target/classes/view/GameHouse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kelompok_tiga.gamehouse;
+
+/**
+ *
+ * @author m3g3n
+ */
+public class GameHouse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
